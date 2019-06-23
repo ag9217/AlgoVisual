26d966d161f893f68787b6efd74a968cc0196b67
ag9217 <ag9217@ic.ac.uk>
2019-06-23 22:18:24 +0100

    created randomise and print func

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <stdio.h>
+
+//funcion prototypes
+void randomizer(int* array, int n);
+void print(int* array, int n);
+void BubbleSort(int *array, int n);
 
 int main()
 {
-    std::cout << "Hello world" << std::endl; //test code
-    return 0;
+    int array[9];
+
+   randomizer(array, 9);
+   print(array, 9);
+   BubbleSort(array, 9);
+   print(array, 9);
+
+   return 0;
+}
+
+void randomizer(int* array, int n)
+{
+   srand(time(0));
+
+   //randomiser
+   for(int i = 0; i < n; i++)
+   {
+      array[i] = rand() % 10 + 1;
+   }
+}
+
+void print(int *array, int n)
+{
+
+   std::cout << "[";
+
+   for(int i = 0; i < n; i++)
+   {
+      if(i < n - 1)
+      {
+         std::cout << array[i] << ",";
+      }
+      else
+      {
+         std::cout << array[i] << "]" << std::endl;
+      }
+   }
 }

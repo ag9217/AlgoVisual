09b105b80b65c2fdfd58c20d85e6c861d7eb23a3
Armand <ag9217@ic.ac.uk>
2019-06-28 01:18:45 +0200

    added more comments

--- include/algo.hpp
+++ include/algo.hpp
@@ -1,3 +1,4 @@
+//this file contains all the algorithms used in this program
 //function prototypes
 void swap(int &a, int &b);
 void BubbleSort(std::vector<int>& array);
--- include/extra.hpp
+++ include/extra.hpp
@@ -1,3 +1,4 @@
+//this file contains functions that are used on the randoimised vectors
 //function prototypes
 void randomizer(std::vector<int>& array, int n);
 void print(std::vector<int>& array);

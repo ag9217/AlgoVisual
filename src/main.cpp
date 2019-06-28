f3aac0b5298adfc2d8f37e1f66bb115f4afe54cd
Armand <ag9217@ic.ac.uk>
2019-06-28 15:12:24 +0200

    removed test code

--- src/main.cpp
+++ src/main.cpp
@@ -12,25 +12,6 @@
 //-------------MAIN-----------------
 int main()
 {
-
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
    std::vector<int> array;
 
    randomizer(array, 9);
